a5c407e3715910568cf019fed987f7444bc19be4
Oscar Brenes <oscar.brenes@hangarww.com>
2022-12-28 16:37:17 -0600

    updating styling naming

--- src/components/Content.js
+++ src/components/Content.js
@@ -11,24 +11,24 @@ export function Content(props) {
   return (
     <>
         <section id="intro" className={currentScene === 0 && !toggleCont ? "active" : "inactive"}>
-            <div className='popup full center headline'>
+            <div className='content full center headline'>
               <h1>Take me to <span>Tokyo</span></h1>
             </div>
             <ScrollDown/>
         </section>
         <section id="cr" className={currentScene === 1 && !toggleCont ? "active" : "inactive"}>
-            <div className='popup third left top'>
+            <div className='content third left top'>
                 <p>Hi! I'm Oscar Brenes a frontend developer from Costa Rica, I have been working for what now it's Critical Mass Latam for the past 7&nbsp;years</p>
             </div>
-            <div className='popup third right bottom'>
+            <div className='content third right bottom'>
                 <p>I have gain experience on different positions and moved from a small design team that supported GoDaddy to a giant tech team that carry on BMW USA website</p>
             </div>
         </section>
         <section id="jpn" className={currentScene === 2 && !toggleCont ? "active" : "inactive"}>
-            <div className='popup third left top'>
+            <div className='content third left top'>
                 <p>3 years ago I visited Japan and fell in love with the culture and the beauty of the country, now I feel that's my time go back and stablish myself</p>
             </div>
-            <div className='popup third right bottom'>
+            <div className='content third right bottom'>
                 <p>Currently I'm looking for a job opportunity that takes me to Tokyo, feel free to reach me out</p>
             </div>
         </section>
