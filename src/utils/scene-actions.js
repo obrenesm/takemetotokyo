7ab6be8767a406ce38d0e065dad0b5f095806024
Oscar Brenes <oscar.brenes@hangarww.com>
2023-01-04 16:53:23 -0600

    notes

--- src/utils/scene-actions.js
+++ src/utils/scene-actions.js
@@ -1,6 +1,5 @@
 import { scenes } from '../data/scenesData';
 
-
 export const SCENE_ACTIONS = {
   NEXT: 'Next',
   BACK: 'Back',
@@ -16,3 +15,15 @@ export function getActionByCursor(currentScene, camDeviation) {
     return camDeviation && camDeviation[0] < 0 ? SCENE_ACTIONS.BACK : SCENE_ACTIONS.NEXT;
   }
 }
+
+//TODO: touch scroll
+
+// export function getActionByTouch(currentScene, camDeviation) {
+//   if (currentScene === 0) {
+//     return SCENE_ACTIONS.NEXT
+//   } else if (currentScene === scenes.length - 1 && camDeviation && camDeviation[0] > 0) {
+//     return SCENE_ACTIONS.RETURN;
+//   } else {
+//     return camDeviation && camDeviation[0] < 0 ? SCENE_ACTIONS.BACK : SCENE_ACTIONS.NEXT;
+//   }
+// }
