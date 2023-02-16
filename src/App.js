d84c447b31fa780c3f2cd1f1fa1cdbd543df8c76
Oscar Brenes <oscar.brenes@hangarww.com>
2023-02-16 17:37:34 -0600

    creating routes and updating resume

--- src/App.js
+++ src/App.js
@@ -7,68 +7,76 @@ import gsap from 'gsap'
 
 import { Canvas } from '@react-three/fiber';
 import React, { useRef, useState, useReducer } from 'react';
-import { OrbitControls, ScrollControls, Scroll, useScroll } from '@react-three/drei';
-import { Scenario } from './components/3d/Scenario';
-import { Content } from './components/Content';
-import { Nav } from './components/Nav';
+import { Main } from './pages/Main'
+import { Resume } from './pages/resume/Resume'
+import { Route, Routes } from 'react-router-dom'
+// import { OrbitControls, ScrollControls, Scroll, useScroll } from '@react-three/drei';
+// import { Scenario } from './components/3d/Scenario';
+// import { Content } from './components/Content';
+// import { Nav } from './components/Nav';
 
-import './App.scss';
-import { Suspense } from 'react';
+// import './App.scss';
+// import { Suspense } from 'react';
 
-import { CamDeviationContext, ContactContext, Context } from './components/ContextProvider';
-import { Resume } from './components/Resume';
-import { Cursor, followCursorEvent } from './components/cursor/Cursor';
-import { initialSceneState, sceneReducer } from './reducers/scene.reducer';
-import { getActionByCursor } from './utils/scene-actions'
-import { CalculateMousePosition } from './components/CalculateMouseDeviation'
+// import { CamDeviationContext, ContactContext, Context } from './components/ContextProvider';
+// import { Resume } from './components/Resume';
+// import { Cursor, followCursorEvent } from './components/cursor/Cursor';
+// import { initialSceneState, sceneReducer } from './reducers/scene.reducer';
+// import { getActionByCursor } from './utils/scene-actions'
+// import { CalculateMousePosition } from './components/CalculateMouseDeviation'
 
-import { isTouchEnabled } from './utils/utils'
-import { onTouchStart, onTouchMove, onTouchEnd } from './utils/scene-touch-actions'
+// import { isTouchEnabled } from './utils/utils'
+// import { onTouchStart, onTouchMove, onTouchEnd } from './utils/scene-touch-actions'
 
 function App() {
-  const [currentScene, setCurrentScene] = useState(0);
-  const [toggleCont, setToggleCont] = useState(false);
-  const [camDeviation, setCamDeviation] = useState([0, 0]);
-  const mouseRef = useRef();
-  const [sceneState, dispatch] = useReducer(sceneReducer, initialSceneState);
-  const [touchStart, setTouchStart] = useState(null)
-  const [touchEnd, setTouchEnd] = useState(null)
+  // const [currentScene, setCurrentScene] = useState(0);
+  // const [toggleCont, setToggleCont] = useState(false);
+  // const [camDeviation, setCamDeviation] = useState([0, 0]);
+  // const mouseRef = useRef();
+  // const [sceneState, dispatch] = useReducer(sceneReducer, initialSceneState);
+  // const [touchStart, setTouchStart] = useState(null)
+  // const [touchEnd, setTouchEnd] = useState(null)
 
   return (
-    <div id="main" style={{ width: '100vw', height: '100vh' }} 
-      onMouseMove={isTouchEnabled() ? undefined : (e) => followCursorEvent(mouseRef)(e)}
-      onTouchStart={(e) => onTouchStart(e, setTouchStart, setTouchEnd)}
-      onTouchMove={(e) => onTouchMove(e, setTouchEnd)}
-      onTouchEnd={(e) => {
-        dispatch({ type: onTouchEnd(sceneState.currentScene, touchStart, touchEnd)})}}
-      >
-      <Context.Provider value={[currentScene, setCurrentScene]}>
-        <CamDeviationContext.Provider value={[camDeviation, setCamDeviation]}>
-          <ContactContext.Provider value={[toggleCont, setToggleCont]}>
-            <Nav/>
-            <Content currentScene={sceneState.currentScene} />
-            {isTouchEnabled() ? undefined : <Cursor currentScene={sceneState.currentScene} ref={mouseRef} />}
+    // <div id="main" style={{ width: '100vw', height: '100vh' }} 
+    //   onMouseMove={isTouchEnabled() ? undefined : (e) => followCursorEvent(mouseRef)(e)}
+    //   onTouchStart={(e) => onTouchStart(e, setTouchStart, setTouchEnd)}
+    //   onTouchMove={(e) => onTouchMove(e, setTouchEnd)}
+    //   onTouchEnd={(e) => {
+    //     dispatch({ type: onTouchEnd(sceneState.currentScene, touchStart, touchEnd)})}}
+    //   >
+    //   <Context.Provider value={[currentScene, setCurrentScene]}>
+    //     <CamDeviationContext.Provider value={[camDeviation, setCamDeviation]}>
+    //       <ContactContext.Provider value={[toggleCont, setToggleCont]}>
+    //         <Nav/>
+    //         <Content currentScene={sceneState.currentScene} />
+    //         {isTouchEnabled() ? undefined : <Cursor currentScene={sceneState.currentScene} ref={mouseRef} />}
 
-            {/* // TODO NOTE: app is reloading all the time, is a resize function necessary? */}
+    //         {/* // TODO NOTE: app is reloading all the time, is a resize function necessary? */}
 
-            <Canvas camera={{ fov: 75, near: 0.1, far: 100, position: [-5, -4, 13] }}
-            onClick={isTouchEnabled() ? undefined : (e) => dispatch({ type: getActionByCursor(sceneState.currentScene, camDeviation)})}
-            >
-              <ambientLight intensity={0.5} />
-              <spotLight position={[200, 300, -100]} angle={0.3} intensity={0.4} />
-              <spotLight position={[-200, -400, 100]} angle={1} intensity={0.2} />
-              <pointLight position={[-235, 235, 0]} intensity={0.2} />
-              <Suspense fallback={null}>
-                <CalculateMousePosition currentScene={sceneState.currentScene}>
-                  <Scenario currentScene={sceneState.currentScene} />
-                </CalculateMousePosition>
-              </Suspense>
-            </Canvas>
-          </ContactContext.Provider>
-        </CamDeviationContext.Provider>
-      </Context.Provider>
-      {/* <Resume/> */}
-    </div>
+    //         <Canvas camera={{ fov: 75, near: 0.1, far: 100, position: [-5, -4, 13] }}
+    //         onClick={isTouchEnabled() ? undefined : (e) => dispatch({ type: getActionByCursor(sceneState.currentScene, camDeviation)})}
+    //         >
+    //           <ambientLight intensity={0.5} />
+    //           <spotLight position={[200, 300, -100]} angle={0.3} intensity={0.4} />
+    //           <spotLight position={[-200, -400, 100]} angle={1} intensity={0.2} />
+    //           <pointLight position={[-235, 235, 0]} intensity={0.2} />
+    //           <Suspense fallback={null}>
+    //             <CalculateMousePosition currentScene={sceneState.currentScene}>
+    //               <Scenario currentScene={sceneState.currentScene} />
+    //             </CalculateMousePosition>
+    //           </Suspense>
+    //         </Canvas>
+    //       </ContactContext.Provider>
+    //     </CamDeviationContext.Provider>
+    //   </Context.Provider>
+    //   {/* <Resume/> */}
+    // </div>
+    <Routes>
+      <Route path="/takemetotokyo" element={<Main/>} />
+
+      <Route path="/takemetotokyo/resume" element={<Resume/>} />
+    </Routes>
   );
 }
 
---
+++ src/pages/resume/Resume.js
@@ -0,0 +1,106 @@
+import React from 'react'
+import './Resume.scss';
+
+export function Resume(props) {
+
+    document.getElementById('root').classList.add('cv')
+
+  return (
+    <>
+        <div id="resume">
+            <div className='thumbnail'>
+                <div className='box'></div>
+            </div>
+            <div className='information'>
+                <section>
+                    <h3>Work Experience</h3>
+                    <div>
+                        <h4>Frontend Developer</h4>
+                        <h5>Critical Mass Latam for BMW USA</h5>
+                        <h6>2021-today</h6>
+                    </div>
+                    <div>
+                        <h4>Content Developer</h4>
+                        <h5>Critical Mass Latam for BMW USA</h5>
+                        <h6>2019-2020</h6>
+                    </div>
+                    <div>
+                        <h4>Production Designer</h4>
+                        <h5>Critical Mass Latam for GoDaddy</h5>
+                        <h6>2016-2018</h6>
+                    </div>
+                    {/* <div>
+                        <h4>Founder and Sport Manager</h4>
+                        <h5>Clean Cycling</h5>
+                        <h6>2014-2017</h6>
+                    </div> */}
+                    <div>
+                        <h4>Freelance Web & Graphic Designer</h4>
+                        <h6>2011-2015</h6>
+                    </div>
+                </section>
+                <section>
+                    <h3>Education</h3>
+                    <div>
+                        <h4>Superior Degree in Sports Management</h4>
+                        <h5>TEC</h5>
+                        <h6>2014-2015</h6>
+                    </div>
+                    <div>
+                        <h4>Superior Degree in Web Design and Development</h4>
+                        <h5>U Creativa</h5>
+                        <h6>2011-2013</h6>
+                    </div>
+                    <div>
+                        <h4>Associate Degree in Graphic Design</h4>
+                        <h5>COVAO</h5>
+                        <h6>2008-2010</h6>
+                    </div>
+                </section>
+                <section>
+                    <h3>Courses</h3>
+                    <div>
+                        <h4>React JS</h4>
+                        <h5>Scrimba</h5>
+                        <h6>2022</h6>
+                    </div>
+                    <div>
+                        <h4>Responsive Web Design Fundamentals</h4>
+                        <h4>HTML and CSS Responsive Images</h4>
+                        <h4>Java Script</h4>
+                        <h5>Udacity</h5>
+                        <h6>2017</h6>
+                    </div>
+                    <div>
+                        <h4>Digital Marketing</h4>
+                        <h4>Vanilla Java Script</h4>
+                        <h4>User Experience</h4>
+                        <h5>La 404</h5>
+                        <h6>2015</h6>
+                    </div>
+                    <div>
+                        <h4>MOOC of Enviromental Physiology</h4>
+                        <h5>University of Melbourne</h5>
+                        <h6>2014</h6>
+                    </div>
+                    <div>
+                        <h4>MOOC of Excercise Physiology</h4>
+                        <h5>Stanford University</h5>
+                        <h6>2013</h6>
+                    </div>
+                    <div>
+                        <h4>MOOC of Nutrition and Physical Activity</h4>
+                        <h5>University of Pittsburgh</h5>
+                        <h6>2013</h6>
+                    </div>
+                    <div>
+                        <h4>Level 1 UCI Cycling Coaching</h4>
+                        <h5>UCI</h5>
+                        <h6>2003</h6>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </>
+  )
+}
