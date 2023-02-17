9a470388be4191bf9b55ac342cee0d25d559b017
Oscar Brenes <oscar.brenes@hangarww.com>
2023-02-16 21:20:56 -0600

    adding external links to the modal

--- src/components/contactme/ContactMe.js
+++ src/components/contactme/ContactMe.js
@@ -14,12 +14,16 @@ export function ContactMe(props) {
         // open={toggleCont}
         >
           <div className='modal-container'>
-            <h2>Let's talk!</h2>
-            <p>Here goes a text that say something catchy.</p>
-            <a href="mailto:exor.os car@gmail.com" className='mail'>exor.oscar@gmail.com</a>
-            <a href="tel:+50688117533" className='tel'>+506 88 11 75 33</a>
-
-            <a href="/takemetotokyo/resume" className='resumeto'>Resume</a>
+            <section>
+              <h2>Let's talk!</h2>
+              <p>Here goes a text that say something catchy.</p>
+              <a href="mailto:exor.os car@gmail.com" className='mail'>exor.oscar@gmail.com</a>
+              <a href="tel:+50688117533" className='tel'>+506 88 11 75 33</a>
+            </section>
+            <section className='extLinks'>
+              <a href="/takemetotokyo/resume" target="_blank" className='external'>Resume</a>
+              <a href="https://www.linkedin.com/in/obrenesm" rel="noopener noreferrer" target="_blank" className='external'>LinkedIn</a>
+            </section>
           </div>
         </section>
     </>
