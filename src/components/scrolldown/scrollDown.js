22d1f11189bb6b194e952f3a45dfb6c55dbbbaa1
Oscar Brenes <oscarbrenes@Oscars-MacBook-Pro.local>
2023-03-20 13:26:29 -0600

    updating scroll copy and unblocking style to mix devices

--- src/components/scrolldown/scrollDown.js
+++ src/components/scrolldown/scrollDown.js
@@ -4,7 +4,7 @@ export function ScrollDown() {
     return (
         <>
             <div className='scroll'>
-                <h6>swipe to scroll down</h6>
+                <h6>swipe to scroll</h6>
                 <div className='arrow'></div>
             </div>
         </>
