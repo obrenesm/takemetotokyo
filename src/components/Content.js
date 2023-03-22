feea409823634d2cb3c05f79ae878f46cfc705aa
Oscar Brenes <oscarbrenes@Oscars-MacBook-Pro.local>
2023-03-21 22:36:38 -0600

    copy update

--- src/components/Content.js
+++ src/components/Content.js
@@ -22,7 +22,7 @@ export function Content({currentScene}) {
         <section id="cr" className={currentScene === 1 && !toggleCont ? "active" : "inactive"}>
             <div className='content third left top'>
                 <p>
-                    Hi! I'm Oscar, a detail-oriented frontend developer from Costa Rica. I've been working for <Tooltip content="Previously known as Hangar, latter Hangar Worldwide, and recently fully integrated as part of Critical Mass" direction="down">Critical Mass</Tooltip>the past 7 years and it has been a great journey, learning more about my skills and honing my craft.
+                    Hi! I'm Oscar, a detail-oriented frontend developer from Costa Rica. I have been working for <Tooltip content="Previously known as Hangar, latter Hangar Worldwide, and recently fully integrated as part of Critical Mass" direction="down">Critical Mass</Tooltip>the past 7 years and it has been a great journey, learning more about my skills and honing my craft.
                 </p>
             </div>
             <div className='content third right bottom'>
