7d07bdf52e7300e539fd3f0c69b3211acb1f955c
Oscar Brenes <oscarbrenes@Oscars-MacBook-Pro.local>
2025-10-23 05:47:30 -0600

    updating meta description

--- src/components/Content.js
+++ src/components/Content.js
@@ -22,19 +22,19 @@ export function Content({currentScene}) {
         <section id="cr" className={currentScene === 1 && !toggleCont ? "active" : "inactive"}>
             <div className='content third left top'>
                 <p>
-                    Hi! I'm Oscar, from Costa Rica. Until a while ago I was CTO at Buzz, previously I worked for more than seven years for <Tooltip content="Previously Hangar, latter Hangar Worldwide, and latter fully integrated as part of Critical Mass" direction="down">Critical Mass</Tooltip>
+                    Hi! I'm Oscar, from Costa Rica. Until recently, I was the CTO at Buzz, and before that, I spent over seven years at <Tooltip content="Previously Hangar, latter Hangar Worldwide, and latter fully integrated as part of Critical Mass" direction="down">Critical Mass</Tooltip>
                 </p>
             </div>
             <div className='content third right bottom'>
-                <p>I have grown a lot learning from my different roles, from a small design team providing creative services to a huge tech team that supports the BMW USA website, and latter in charge of a whole department. But, it's time for my next big step: Japan.</p>
+                <p>Over the years, I've worn many hats — from being part of a small, creative design team to joining the tech crew behind the BMW USA website, and later leading an entire department. Now, I'm ready for my next big chapter: Japan.</p>
             </div>
         </section>
         <section id="jpn" className={currentScene === 2 && !toggleCont ? "active" : "inactive"}>
             <div className='content third left top'>
-                <p>8 years ago, I visited Japan for the first time and was captivated by the culture. Since then, Now I have a year living here under a language student visa.</p>
+                <p>I first visited Japan six years ago and instantly fell in love with its culture. Last year, I came back — this time as a language student — and it confirmed what I already knew: I want to build a long-term life here.</p>
             </div>
             <div className='content third right bottom'>
-                <p>But I'm looking forward to establish long-term in this beautiful country. It doesn't matter if you have a business proposal, a job offer, or just want to say hi, feel free to <span onClick={toggleContactState} className={'modal-cta'}>reach&nbsp;out.</span></p>
+                <p>Whether you’ve got a project idea, a job opportunity, or just want to say hi, feel free to <span onClick={toggleContactState} className={'modal-cta'}>reach&nbsp;out.</span></p>
             </div>
         </section>
     </>
